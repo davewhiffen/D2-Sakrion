9a23d8ffa0fe95f1791668b3544319fc6ac70f52
Dave Whiffen <david.whiffen@ryerson.ca>
2020-04-14 08:10:01 -0400

    Added Title Menu, Added Loading Zones
    Added Title Menu
    
    -Play button and title, music, credits
    
    Loading Zones
    
    -Now the level doesn't load the entire map at once.
    -Only areas that you need are loaded, and new areas load in as you move through the level.
    
    There is a bit of a slowdown when you hit these, but the alternative was that the game run at 15fps for the entire time, so this is a much better solution.
    
    Boss Tweaks
    
    -Boss' health has been decreased to not have the fight drag on too long
    
    Enemy Wave Tweaks
    
    -In Room 3, enemies now spawn in 2 waves.
    -All first floor enemies are part of the initial boss fight.
    -When the boss is killed, enemies on the second and third floor spawn in.
    
    -This prevents the game from dramatically slowing down, but it also makes the fight more manageable and not mayhem.  There were actually TOO MANY enemies spawning in at once for it to even feel good to play.
    
    Co-Authored-By: daimenion <daimenion@users.noreply.github.com>

---
+++ Assets/Scripts/SetActive.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SetActive : MonoBehaviour
+{
+    public GameObject room;
+    public GameObject[] OtherRooms;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            room.SetActive(true);
+            for (int i = 0; i < OtherRooms.Length; i++)
+            {
+                OtherRooms[i].SetActive(false);
+            }
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
