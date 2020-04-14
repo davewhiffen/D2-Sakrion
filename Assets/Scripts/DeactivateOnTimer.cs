0cb0ec7865edec89fd6e99f87e6e36ab91dd139b
Dave Whiffen <david.whiffen@ryerson.ca>
2020-04-14 16:48:39 -0400

    Changes from Today

---
+++ Assets/Scripts/DeactivateOnTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateOnTimer : MonoBehaviour
+{
+    public float sec = 14f;
+    void Start()
+    {
+       
+
+        StartCoroutine(LateCall());
+    }
+
+    IEnumerator LateCall()
+    {
+
+        gameObject.SetActive(true);
+        //Do Function here...
+
+
+        yield return new WaitForSeconds(sec);
+
+        gameObject.SetActive(false);
+        //Do Function here...
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
